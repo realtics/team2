9621bee95ed580095f77049789efe8d6eb4caa5f
Ahn <quddnr0512@gmail.com>
2019-11-18 22:31:09 +0900

    움직임 조이스틱 추가

---
+++ Client/Assets/Scripts/00_Common/InputInMobile/MovementJoystick.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public enum JoystickDirection
+{
+    Top,
+    RightTop,
+    Right,
+    RightBottom,
+    Bottom,
+    LeftBottom,
+    Left,
+    LeftTop
+}
+
+public class MovementJoystick : MonoBehaviour
+{
+    private const float DirectionAngle = 45.0f / 2.0f;
+    private Transform _stick;
+
+    private Vector3 _initPos;
+    private float _radius;
+
+    void Start()
+    {
+        _stick = transform.GetChild(0);
+        _initPos = _stick.GetComponent<RectTransform>().position;
+        _radius = GetComponent<RectTransform>().sizeDelta.x / 6.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Drag(BaseEventData eventData)
+    {
+        PointerEventData data = eventData as PointerEventData;
+        Vector3 touchPos = data.position;
+        Vector3 direction = Vector3.Normalize(touchPos - _initPos);
+        float distance = Vector3.Distance(touchPos, _initPos);
+        direction = CalcStickDirection(direction);
+        _stick.position = _initPos + (direction * _radius);
+    }
+
+    // 드래그 끝.
+    public void DragEnd()
+    {
+        _stick.position = _initPos;
+    }
+
+    private Vector2 CalcStickDirection(Vector3 direction)
+    {
+        Vector2 stickDir = Vector2.zero;
+        float angle = Quaternion.FromToRotation(Vector3.right, direction).eulerAngles.z;
+
+        if (angle > DirectionAngle && angle < 90.0f - DirectionAngle)
+            stickDir = new Vector2(1.0f, 1.0f);
+        else if (angle > 90.0f - DirectionAngle && angle < 135.0f - DirectionAngle)
+            stickDir = new Vector2(0.0f, 1.0f);
+        else if (angle > 135.0f - DirectionAngle && angle < 180.0f - DirectionAngle)
+            stickDir = new Vector2(-1.0f, 1.0f);
+        else if (angle > 180.0f - DirectionAngle && angle < 225.0f - DirectionAngle)
+            stickDir = new Vector2(-1.0f, 0.0f);
+        else if (angle > 225.0f - DirectionAngle && angle < 270.0f - DirectionAngle)
+            stickDir = new Vector2(-1.0f, -1.0f);
+        else if (angle > 270.0f - DirectionAngle && angle < 315.0f - DirectionAngle)
+            stickDir = new Vector2(0.0f, -1.0f);
+        else if (angle > 315.0f - DirectionAngle && angle < 360.0f - DirectionAngle)
+            stickDir = new Vector2(1.0f, -1.0f);
+        else
+            stickDir = new Vector2(1.0f, 0.0f);
+
+        return stickDir;
+    }
+}
