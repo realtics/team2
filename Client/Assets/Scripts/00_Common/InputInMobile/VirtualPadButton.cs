2369a3067e5e0d1ccfd35944d7ef2a3212ced859
Ahn <quddnr0512@gmail.com>
2019-11-18 23:34:27 +0900

    더미 조이스틱 구현
    이동 점프 공격

---
+++ Client/Assets/Scripts/00_Common/InputInMobile/VirtualPadButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VirtualPadButtonType
+{
+    Attack,
+    Jump,
+    Run,
+}
+
+public class VirtualPadButton : MonoBehaviour
+{
+    [SerializeField]
+    private VirtualPadButtonType _type;
+    private PlayerCharacter _pc;
+
+    private void Start()
+    {
+        _pc = FindObjectOfType<PlayerCharacter>();
+    }
+
+    public void OnClick()
+    {
+        ButtonAction();
+    }
+
+    public void ButtonAction()
+    {
+        switch (_type)
+        {
+            case VirtualPadButtonType.Attack:
+                _pc.SetAttack();
+                break;
+            case VirtualPadButtonType.Jump:
+                _pc.SetJump();
+                break;
+            case VirtualPadButtonType.Run:
+                _pc.SetRun();
+                break;
+        }
+    }
+}
