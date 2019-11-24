903ad8a0178843220a5a5737d235e2a11065c002
Dongnehye <cmmn123@gmail.com>
2019-11-25 01:59:03 +0900

    정리

--- Client/Assets/Scripts/00_Common/DungeonManager.cs
+++ Client/Assets/Scripts/00_Common/DungeonManager.cs
@@ -5,72 +5,66 @@ using UnityEditor;
 
 public class DungeonManager : MonoBehaviour
 {
-    //[SerializeField]
-    //private Potal[] _potals;
-    //private Potal _currentPotal;
-    //private ARROW _arrow;
+    [SerializeField]
+    private Potal[] _potals;
+    private Potal _currentPotal;
+    private ARROW _arrow;
 
     //private JsonManagement _jsonManagement;
 
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-    //    FIndPotals();
-    //    _jsonManagement = new JsonManagement();
-    //}
+    // Start is called before the first frame update
+    void Start()
+    {
+        FIndPotals();
+        //_jsonManagement = new JsonManagement();
+    }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if(FindActiveCurrentPotal())
-    //    {
-    //        DestroyAllFieldObject();
+    // Update is called once per frame
+    void Update()
+    {
+        if (FindActiveCurrentPotal())
+        {
+            DestroyAllFieldObject();
 
-    //        LoadPotalDungeon();
+            LoadPotalDungeon();
 
-    //        TeardownPotals();
-    //    }
-    //}
-    //public void SaveDungen()
-    //{
-    //    // Test.. 
-    //    _jsonManagement.JsonSave();
-        
-    //}
-    //private void LoadPotalDungeon()
-    //{
-    //    //JsonData data = _jsonManagement.JsonLoad<JsonData>(_currentPotal.crossDungenName);
-    //    //GameObject obj = (GameObject)Resources.Load(data.filePath);
-    //    //obj.transform.position = data.position;
-    //    //Instantiate(obj);
-    //}
+            TeardownPotals();
+        }
+    }
+    private void LoadPotalDungeon()
+    {
+        //JsonData data = _jsonManagement.JsonLoad<JsonData>(_currentPotal.crossDungenName);
+        //GameObject obj = (GameObject)Resources.Load(data.filePath);
+        //obj.transform.position = data.position;
+        //Instantiate(obj);
+    }
 
-    //private void FIndPotals()
-    //{
-    //   _potals = FindObjectsOfType<Potal>();
-    //}
-    //private bool FindActiveCurrentPotal()
-    //{
-    //    for (int i = 0; i < _potals.Length; i++)
-    //    {
-    //        if (_potals[i].IsPlayerEnter)
-    //        {
-    //            _currentPotal = _potals[i];
-    //            return true;
-    //        }
-    //    }
-    //    return false;
-    //}
+    private void FIndPotals()
+    {
+        _potals = FindObjectsOfType<Potal>();
+    }
+    private bool FindActiveCurrentPotal()
+    {
+        for (int i = 0; i < _potals.Length; i++)
+        {
+            if (_potals[i].IsPlayerEnter)
+            {
+                _currentPotal = _potals[i];
+                return true;
+            }
+        }
+        return false;
+    }
 
-    //private void TeardownPotals()
-    //{
-    //    for (int i = 0; i < _potals.Length; i++)
-    //    {
-    //        _potals[i].Teardown();
-    //    }
-    //}
-    //private void DestroyAllFieldObject()
-    //{
-        
-    //}
+    private void TeardownPotals()
+    {
+        for (int i = 0; i < _potals.Length; i++)
+        {
+            _potals[i].Teardown();
+        }
+    }
+    private void DestroyAllFieldObject()
+    {
+
+    }
 }
