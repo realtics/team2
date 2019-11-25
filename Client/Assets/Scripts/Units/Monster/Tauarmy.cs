af12744743c3a5b9ca30dad9fd02ea8c66939159
SKMBOSS <zzangjungsu@gmail.com>
2019-11-25 18:42:31 +0900

    Update Tauarmy.cs
    랜덤 함수로 공격1, 공격2 선택 호출

--- Client/Assets/Scripts/Units/Monster/Tauarmy.cs
+++ Client/Assets/Scripts/Units/Monster/Tauarmy.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Tauarmy : BaseMonster
 {
-    private enum TauAttackMotion
+    private enum TauarmyAttackMotion
     {
         AttackMotion1 = 0,
         AttackMotion2 = 1,
         AttacknMotionEnd = 2
     }
-    private TauAttackMotion _currentAttackMotion;
+    private TauarmyAttackMotion _currentAttackMotion;
 
     protected override void Start()
     {
@@ -24,23 +24,22 @@ public class Tauarmy : BaseMonster
 
     //AttackState
     public override void EnterAttackState()
-    {
-        if (_target == null)
-        {
-            return;
-        }
-
-        IsAttack = true;
-
-        FlipImage();
-        _animator.SetBool("isAttacking", true);
-
+    { 
         base.EnterAttackState();
+        _currentAttackMotion = (TauarmyAttackMotion)Random.Range((int)TauarmyAttackMotion.AttackMotion1, (int)TauarmyAttackMotion.AttacknMotionEnd);
+        _animator.SetInteger("attackMotion", (int)_currentAttackMotion);
+        SetForwardDirection();
+
     }
 
     public override void UpdateAttackState()
     {
         base.UpdateAttackState();
+        if (_currentAttackMotion == TauarmyAttackMotion.AttackMotion2)
+        {
+            transform.position += _forwardDirection * Time.deltaTime * (_moveSpeed*3);
+        }
+        
     }
 
     public override void ExitAttackState()
