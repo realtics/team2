8e6fc4489d2e5b778b584f1e5960e72da9944e66
SKMBOSS <zzangjungsu@gmail.com>
2019-11-28 12:53:17 +0900

    Update BaseMonster.cs
    죽음알림 가상화

--- Client/Assets/Scripts/Units/Monster/BaseMonster.cs
+++ Client/Assets/Scripts/Units/Monster/BaseMonster.cs
@@ -389,6 +389,11 @@ public class BaseMonster : MonoBehaviour
         //nothing
     }
 
+    public void ChangeDieState()
+    {
+        ChangeState(_dieState);
+    }
+
     //HitState
     public virtual void EnterHitState()
     {
@@ -570,7 +575,7 @@ public class BaseMonster : MonoBehaviour
     }
 
     //For MonsterManager
-    public void NoticeDie()
+    public virtual void NoticeDie()
     {
         MonsterManager.Instance.ReceiveMonsterDie(this);
     }
