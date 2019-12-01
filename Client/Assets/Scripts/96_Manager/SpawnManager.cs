2c88808aedfcc1fc247bbe0affddb5ffb1c1050a
Dongnehye <cmmn123@gmail.com>
2019-12-01 23:52:02 +0900

    MapLoader 리팩토링

--- Client/Assets/Scripts/96_Manager/SpawnManager.cs
+++ Client/Assets/Scripts/96_Manager/SpawnManager.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private List<GameObject> _dungeonGameObject;
+
     private static SpawnManager _instacne;
     public static SpawnManager instacne
     {
@@ -11,19 +16,94 @@ public class SpawnManager : MonoBehaviour
             return _instacne;
         }
     }
+
+
+    private Dictionary<string, GameObject> _cache = new Dictionary<string, GameObject>();
+
     // Use this for initialization
     void Start()
     {
         _instacne = this;
+        _dungeonGameObject = new List<GameObject>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject LoadResourceFromCache(string path)
     {
-
+        GameObject resourceObj = null;
+        _cache.TryGetValue(path, out resourceObj);
+        if (resourceObj == null)
+        {
+            resourceObj = Resources.Load<GameObject>(path);
+            if (resourceObj != null)
+            {
+                _cache.Add(path, resourceObj);
+            }
+        }
+        return resourceObj;
     }
-    public void DungeonObjectInstantiate()
-    {
 
+    public void ClearCache()
+    {
+        _cache.Clear();
+        Resources.UnloadUnusedAssets();
+    }
+
+    public void Instantiate(DungeonInfo dungeon)
+    {
+        foreach (var item in dungeon.objectinfos)
+        {
+            GameObject obj = LoadResourceFromCache(item.filePath);
+            obj = AddObject(obj, item.position);
+
+            _dungeonGameObject.Add(obj);
+        }
+
+        foreach (var item in dungeon.monsterInfos)
+        {
+            GameObject obj = LoadResourceFromCache(item.filePath);
+            MonsterManager.Instance.AddMonster(obj, item.position);
+        }
+
+        foreach (var item in dungeon.potalTransportinfos)
+        {
+            GameObject obj = LoadResourceFromCache(item.filePath);
+            obj = AddObject(obj, item.position);
+
+            PotalTransport potal = obj.GetComponent<PotalTransport>();
+            potal.arrow = item.arrow;
+            potal.nextIndex = item.nextIndex;
+
+            for (int i = 0; i < item.spotPosition.Length; i++)
+            {
+                potal.spotGatePosition[i].position = item.spotPosition[i];
+            }
+
+            _dungeonGameObject.Add(obj);
+        }
+        foreach (var item in dungeon.potalSceneInfos)
+        {
+            GameObject obj = LoadResourceFromCache(item.filePath);
+            obj = AddObject(obj, item.position);
+
+            PotalScene potal = obj.GetComponent<PotalScene>();
+            potal.arrow = item.arrow;
+            potal.nextSceneName = item.nextDataName;
+
+            _dungeonGameObject.Add(obj);
+        }
+        ClearCache();
+    }
+    public GameObject AddObject(GameObject prefab, Vector3 position)
+    {
+        GameObject obj = ObjectPoolManager.Instance.GetRestObject(prefab);
+        obj.transform.position = position;
+        return obj;
+    }
+    public void RoomSetActive(bool active, int index)
+    {
+        foreach (var item in _dungeonGameObject)
+        {
+            item.gameObject.SetActive(active);
+        }
     }
 }
