fd009a97b85067f5a81f24edbdb34e190feba0ef
abu0512 <quddnr0512@gmail.com>
2019-12-02 18:47:57 +0900

    블라슈 터지는거까지 완성 없애는거 해야됨

---
+++ Client/Assets/Scripts/00_Common/SpriteOrderSetterAtSelf.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOrderSetterAtSelf : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+	private int _originLayer;
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+		_originLayer = _renderer.sortingOrder;
+
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _renderer.sortingOrder = -(int)(transform.position.y * 10.0f) + _originLayer;
+    }
+}
