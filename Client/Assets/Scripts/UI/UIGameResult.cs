86dbd4c5af87fce26d7f3dcf795a4958a2052c66
Dongnehye <cmmn123@gmail.com>
2019-12-02 17:08:28 +0900

    결과창 카운트다운 수정

--- Client/Assets/Scripts/UI/UIGameResult.cs
+++ Client/Assets/Scripts/UI/UIGameResult.cs
@@ -10,7 +10,9 @@ public class UIGameResult : MonoBehaviour
     [SerializeField]
     private Image _openResultBox;
     [SerializeField]
-    private Text _countDown;
+    private Image _countDown;
+    [SerializeField]
+    private Sprite[] _countImage;
 
     private Image[] _resultBoxImage;
     private Button[] _resultBoxButton;
@@ -33,7 +35,9 @@ public class UIGameResult : MonoBehaviour
 
     public void SetTime(int time)
     {
-        _countDown.text = time.ToString();
+        if (time > 3)
+            time = 3;
+        _countDown.sprite = _countImage[time];
     }
     // ToDo. 클릭시에 GameManager에서 UI를 호출해서 Sprite 변경 및 임시 아이템 띄우기.
     public void OpenResultBox(int index)
