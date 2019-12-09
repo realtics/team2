e811efc2569c9c60f9c9e5d5e1b53722749ef526
abu0512 <quddnr0512@gmail.com>
2019-12-09 14:27:56 +0900

    스킬쓰고 움직이는거 수정

---
+++ Client/Assets/Scripts/00_Common/JumpAttackEndChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAttackEndChecker : StateMachineBehaviour
+{
+	private BaseUnit _unit;
+	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		if (_unit != null)
+			return;
+
+		_unit = animator.transform.root.GetComponent<BaseUnit>();
+	}
+	public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		_unit.StopAttack();
+	}
+}
