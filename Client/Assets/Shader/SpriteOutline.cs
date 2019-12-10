ff99317cf1de063ade05d556067108d2b810febd
SKMBOSS <zzangjungsu@gmail.com>
2019-12-10 16:53:35 +0900

    Update SpriteOutline.cs
    슈퍼아머 색 보간
    
    +세컨드컬러

--- Client/Assets/Shader/SpriteOutline.cs
+++ Client/Assets/Shader/SpriteOutline.cs
@@ -5,37 +5,69 @@
 public class SpriteOutline : MonoBehaviour
 {
     public Color color = Color.white;
+	public Color secondColor = Color.white;
+	private Color32 lerpedColor;
+	[Range(0, 25)]
+	public int outlineSize = 1;
 
-    [Range(0, 25)]
-    public int outlineSize = 1;
+	private const float _changeTime = 1f;
+	private float _currentTime = 0.0f;
+	private bool _isChangeColor = false;
 
     private SpriteRenderer _spriteRenderer;
 
-    void OnEnable()
+    private void OnEnable()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
 
         UpdateOutline(true);
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         UpdateOutline(false);
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        UpdateOutline(true);
+		ChanageColor();
+		UpdateOutline(true);
     }
 
-    void UpdateOutline(bool outline)
+    private void UpdateOutline(bool outline)
     {
         MaterialPropertyBlock mpb = new MaterialPropertyBlock();
         _spriteRenderer.GetPropertyBlock(mpb);
         mpb.SetFloat("_Outline", outline ? 1f : 0);
-        mpb.SetColor("_OutlineColor", color);
+
+		if (_isChangeColor)
+		{
+			lerpedColor = Color32.Lerp(color, secondColor, _currentTime);
+			mpb.SetColor("_OutlineColor", lerpedColor);
+		}
+		else
+		{
+			lerpedColor = Color32.Lerp(secondColor, color, _currentTime);
+			mpb.SetColor("_OutlineColor", lerpedColor);
+		}
+
         mpb.SetFloat("_OutlineSize", outlineSize);
         _spriteRenderer.SetPropertyBlock(mpb);
     }
+
+	private void ChanageColor()
+	{
+		_currentTime += Time.deltaTime*2;
+		if (_currentTime >= _changeTime)
+		{
+			if (_isChangeColor)
+				_isChangeColor = false;
+			else
+				_isChangeColor = true;
+
+			_currentTime = 0.0f;
+			lerpedColor = Color.black;
+		}
+	}
 }
 
