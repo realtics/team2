f43de83d122c3b68eaf46e8c84dfab226ff319f1
SKMBOSS <zzangjungsu@gmail.com>
2019-12-17 18:13:39 +0900

    인벤토리

--- Client/Assets/Scripts/UI/Inventory/Item.cs
+++ Client/Assets/Scripts/UI/Inventory/Item.cs
@@ -1,8 +1,20 @@
 ﻿using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu]
 public class Item : ScriptableObject
 {
+	[SerializeField]
+	private string _id;
+	
     public string itemName;
     public Sprite icon;
+
+	public string ID { get { return _id; } }
+
+	private void OnValidate()
+	{
+		string path = AssetDatabase.GetAssetPath(this);
+		_id = AssetDatabase.AssetPathToGUID(path);
+	}
 }
