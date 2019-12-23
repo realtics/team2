dc42c675be1842578b29f8fdaed43b59982640f8
abu0512 <quddnr0512@gmail.com>
2019-12-23 18:55:22 +0900

    dnjfdydlf zjalt

---
+++ Client/Assets/Scripts/00_Common/TextMeshOrderSetter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextMeshOrderSetter : MonoBehaviour
+{
+	public TextMeshPro textMesh;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		textMesh.sortingOrder = -(int)(transform.parent.position.y * 10.0f);
+	}
+
+}
