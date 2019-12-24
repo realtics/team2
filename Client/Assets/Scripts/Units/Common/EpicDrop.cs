c4703a60c1a2996ccad6635bfb3b1828a3c559e8
SKMBOSS <zzangjungsu@gmail.com>
2019-12-24 12:42:42 +0900

    Update EpicDrop.cs
    아이템 저장 및 트리거 생성

--- Client/Assets/Scripts/Units/Common/EpicDrop.cs
+++ Client/Assets/Scripts/Units/Common/EpicDrop.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 
 public class EpicDrop : MonoBehaviour
 {
-    [SerializeField]
-    private SpriteRenderer _itemIcon;
-    [SerializeField]
-    private SpriteRenderer _itemTextField;
-    [SerializeField]
-    private TextMeshPro _itemText;
+	[SerializeField]
+	private SpriteRenderer _itemIcon;
+	[SerializeField]
+	private SpriteRenderer _itemTextField;
+	[SerializeField]
+	private TextMeshPro _itemText;
+
+	private Item _item;
 
     public void SetHellItem(Item item)
     {
         _itemIcon.sprite = item.icon;
         _itemText.text = item.itemName;
         _itemTextField.transform.localScale = new Vector3(item.itemName.Length/2, 1, 1);
+
+		_item = item;
     }
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.transform.tag == "UserPlayer")
+		{
+			gameObject.SetActive(false);
+			ItemSaveIO.SaveResultItem(_item.ID);
+		}
+	}
 }
