4fa983ac265712cab0e875a7e5ff641700aef95b
Dongnehye <cmmn123@gmail.com>
2019-12-26 16:59:55 +0900

    윈도우 버전에서도 작동 되게함

--- Client/Assets/Editor/Assetbundle/AssetBundleBuilder.cs
+++ Client/Assets/Editor/Assetbundle/AssetBundleBuilder.cs
@@ -3,10 +3,15 @@ using UnityEditor;
 
 public class AssetBundleBuilder : ScriptableObject
 {
-    [MenuItem("Assets/BuildAssetBundle")]
-    static void BuildBundle()
+    [MenuItem("Assets/BuildAndroidAssetBundle")]
+    static void BuildAndroidBundle()
     {
-        BuildPipeline.BuildAssetBundles("Assets\\StreamingAssets",
-            BuildAssetBundleOptions.None, BuildTarget.Android);
+        BuildPipeline.BuildAssetBundles("Assets\\StreamingAssets\\Android", BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+
+    [MenuItem("Assets/BuildWindowAssetBundle")]
+    static void BuildWindowBundle()
+    {
+        BuildPipeline.BuildAssetBundles("Assets\\StreamingAssets\\StandaloneWindows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
     }
 }
