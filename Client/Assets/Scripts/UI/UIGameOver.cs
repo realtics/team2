82746d89a4ded9f51bff5df7dd180d544e6635e6
Dongnehye <cmmn123@gmail.com>
2019-12-26 16:01:52 +0900

    부활 추가

--- Client/Assets/Scripts/UI/UIGameOver.cs
+++ Client/Assets/Scripts/UI/UIGameOver.cs
@@ -9,19 +9,12 @@ public class UIGameOver : MonoBehaviour
     [SerializeField]
     private Image _countDown;
     [SerializeField]
-    private Button _coinUseButton;
-    [SerializeField]
     private Sprite[] _countDownSprite;
 
     private int _coin;
     private int _time;
 
     private const int _maxTime = 10;
-
-    private void Start()
-    {
-        _coinUseButton.onClick.AddListener(() => { });
-    }
 
     public int Coin
     {
