cab62592a0198f0baea7f6fb765d4c43346f2b4a
Ahn <quddnr0512@gmail.com>
2019-12-30 03:57:50 +0900

    코인 사용시 데미지 주는거 구현중

---
+++ Client/Assets/Scripts/01_Unit/CoinCryAttackBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCryAttackBox : MonoBehaviour
+{
+    [SerializeField]
+    private AttackInfoSender _sender;
+
+    private void Awake()
+    {
+        _sender.Attacker = transform;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.tag.Contains("MonsterHitBox"))
+            return;
+
+        other.transform.root.GetComponent<BaseMonster>().OnHit(_sender);
+    }
+}
